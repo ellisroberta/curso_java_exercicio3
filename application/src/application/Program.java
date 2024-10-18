dc16babd869707c84c8a0930182075623fea35e1
ellisroberta <ellisroberta@hotmail.com>
2024-10-18 16:01:52 -0300

    organizacao das pastas, edicao do .gitignore e realizacao do exercicio

---
+++ application/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Student;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        student.name = sc.nextLine();
+        student.grade1 = sc.nextDouble();
+        student.grade2 = sc.nextDouble();
+        student.grade3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE = %.2f%n", student.finalGrade());
+        System.out.println(student.result());
+
+        sc.close();
+    }
+}
